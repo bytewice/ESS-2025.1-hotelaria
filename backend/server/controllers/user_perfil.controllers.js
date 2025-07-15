1b7f686856e7afa797ddf9c956a595c082bf3c47
DaviGGuerreiro <dggb@cin.ufpe.br>
2025-07-15 09:25:46 -0300

    feat(user_perfil.controllers.js): adding controller functions

--- backend/server/controllers/user_perfil.controllers.js
+++ backend/server/controllers/user_perfil.controllers.js
@@ -1,47 +1,141 @@
-import fs from 'fs'
 import generateTokenAndSetCookie from '../utils/generateToken'
+import User from '../models/user.model.js'
+import UserComum from '../models/user_comum.model.js'
 import bcrypt from 'bcryptjs'
 
-export const getAllUser = (req, res) => {
+export const signupUser = async(req, res) => {
     try{
 
-        {}
+        const {Name, Email, CPF, Password, ConfirmPassword, Telefone} = req.body
+
+        if(Password !== ConfirmPassword){
+            console.log("Password don't match");
+            return res.status(400).json({
+                error: "Password don't match"
+            })
+        }
+
+        const user = await User.findOne({ CPF })
+
+        if(user){
+            console.log("User already exist")
+            return res.status(400).json({
+                error: "User already exist"
+            })
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(Password, salt);
+
+        const newUser = new UserComum({
+            Name,
+            Email,
+            CPF,
+            password: hashedPassword,
+            Telefone,
+            Avaliacoes: [],
+            Metodos: [],
+        })
+
+        if (newUser) {
+            generateTokenAndSetCookie(newUser._id, res)
+            await newUser.save()
+            res.status(201).json({
+                _id: newUser._id,
+                Name: newUser.Name,
+                Email: newUser.Email,
+                CPF: newUser.CPF,
+                Telefone: newUser.Telefone,
+            })
+        }
 
     } catch (error) {
-        console.log("Error in getAllUser:", error.message)
+        console.log("Error in signup:", error.message)
         res.status(500).json({
             error: "Internal Server Error"
         })
     }
 }
 
-export const getUser = (req, res) => {
+export const loginUser = async(req, res) => {
     try{
+        const {CPF, Password} = req.body
+        const user = await UserComum.findOne({ CPF })
+        const isPasswordCorrect = await bcrypt.compare(Password, user?.Password || "")
 
-    } catch (error) {
-        console.log("Error in getUser:", error.message)
+        if (!user || !isPasswordCorrect) { //user nn existe ou senha errada
+            console.log("Invalid credentials")
+            return res.status(400).json({
+                error: "Invalid credentials"
+            })
+        }
+        generateTokenAndSetCookie(user._id, res)
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username
+        })
+
+    } catch(error){
+        console.log("Error in login:", error.message)
         res.status(500).json({
             error: "Internal Server Error"
         })
     }
 }
 
-export const createUser = (req, res) => {
+export const updateUser = async(req,res) => {
     try{
+        const userId = req.params.id
+        const updates = { ...req.body }
 
-    } catch (error) {
-        console.log("Error in createUser:", error.message)
+        if (updates.Password) {
+            updates.Password = await bcrypt.hash(updates.Password, 10)
+        }
+
+        const updatedUser = await UserComum.findByIdAndUpdate(
+            userId,
+            updates,
+            { new: true }
+        )
+        if (!updatedUser) {
+            return res.status(404).json({ error: "User not found" })
+        }
+        res.status(200).json({message:"User updated", user:updatedUser})
+
+    } catch(error){
+        console.log("Error in Update:", error.message)
         res.status(500).json({
             error: "Internal Server Error"
         })
     }
 }
 
-export const updateUser = (req, res) => {
+export const deleteUser = async(req, res) => {
     try{
+        const UserId = req.params.id
+        const user = await UserComum.findByIdAndDelete(UserId)
+        if(!user){
+            return res.status(404).json({ error: "User not found" })
+        }
+        res.status(200).json({message:"User sucessfully deleted"})
+    } catch(error){
+        console.log("Error in deleteUser:", error.message)
+        res.status(500).json({
+            error: "Internal Server Error"
+        })
+    }
 
+}
+
+export const logoutUser = (req, res) => {
+    try {
+        res.cookie("jwt", "", { maxAge:0})
+        res.status(200).json({
+            message: "Logged out successfully"
+        })
     } catch (error) {
-        console.log("Error in updateUser:", error.message)
+        console.log("Error in logout:", error.message)
         res.status(500).json({
             error: "Internal Server Error"
         })
