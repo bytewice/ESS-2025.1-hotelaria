aeabc55fc78240ff0cfeba654228a8ac342fc554
iva <iva@g3c17.cin.ufpe.br>
2025-08-13 19:58:24 -0300

    init cypress config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,21 +3,23 @@ import Home from "./pages/Home";
 import Reservas from "./pages/Reservas";
 import Quartos from "./pages/Quartos";
 import Atrações from "./pages/Atrações";
+import AdminAttractions from "./pages/AdminAttractions";
 import AtracaoDetalhe from "./pages/AtraçãoDetalhe";
 import Layout from "./Layout"; // Caminho para o Layout
 
 export default function App() {
-  return (
+  return (  
     <Router>
       <Routes>
         {/* Home sem Layout */}
         <Route path="/" element={<Home />} />
 
         {/* Páginas secundárias com Layout */}
+        <Route path="/adminatrações" element={<AdminAttractions/>}/>
         <Route path="/reservas" element={<Layout><Reservas /></Layout>} />
         <Route path="/quartos" element={<Layout><Quartos /></Layout>} />
-        <Route path="/atrações" element={<Layout><Atrações /></Layout>} />
-        <Route path="/atrações/:name" element={<Layout><AtracaoDetalhe /></Layout>} />
+        <Route path="/atracoes" element={<Layout><Atrações /></Layout>} />
+        <Route path="/atracoes/:name" element={<Layout><AtracaoDetalhe /></Layout>} />
       </Routes>
     </Router>
   );
