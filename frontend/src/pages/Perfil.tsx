73a3230659d1c36c8f52435faca40eb0168d9b99
Davi Guerreiro <daviguerreiro9a@gmail.com>
2025-08-14 02:53:13 -0300

    feature(Perfil): Adding Perfil Page

--- frontend/src/pages/Perfil.tsx
+++ frontend/src/pages/Perfil.tsx
@@ -1,8 +1,44 @@
+import { useContext } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { UserContext } from "../context/userContext";
+import "../styles/perfil.css"; // Importando nosso novo CSS
+
 export default function Perfil() {
+  const { user } = useContext(UserContext);
+  if (!user) {
+    return <Navigate to="/" />;
+  }
+
+  // Lembre-se que as propriedades (Name, Email, etc.) devem ter o mesmo
+  // nome que você usa no objeto de usuário no seu estado.
   return (
-    <div>
-      <h1>Perfil</h1>
-      <p>Veja as informações de seu usuário.</p>
+    <div className="perfil-container">
+      <div className="perfil-card">
+        <h1>Perfil do Usuário</h1>
+        
+        <div className="perfil-info">
+          <p>
+            <label>Nome:</label>
+            <span>{user.Name}</span>
+          </p>
+          <p>
+            <label>Email:</label>
+            <span>{user.Email}</span>
+          </p>
+          <p>
+            <label>CPF:</label>
+            <span>{user.CPF}</span>
+          </p>
+          <p>
+            <label>Telefone:</label>
+            {/* Mostra 'Não informado' se o telefone não existir */}
+            <span>{user.Telefone || "Não informado"}</span>
+          </p>
+        </div>
+        <Link to="/home" className="perfil-back-button">
+          Voltar para Home
+        </Link>
+      </div>
     </div>
   );
 }
